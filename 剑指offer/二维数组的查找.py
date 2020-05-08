c03b25bfc2ef15625f84a07dd4476c4bcff78a97
Yin-bao <272214368@qq.com>
2020-05-08 13:33:54 +0100

    Update 二维数组的查找.py

--- 剑指offer/二维数组的查找.py
+++ 剑指offer/二维数组的查找.py
@@ -1,5 +1,7 @@
 # 从右下角向左上角查找，这样不会错过目标值。
 # 采用try的形式来判断，若报错则说明越界或者matrix没有值。因此返回False
+# 时间复杂度O(n)
+# 空间复杂度O(1)
 class Solution:
     def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
         n = len(matrix) - 1
