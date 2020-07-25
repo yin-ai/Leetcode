ae7c777a870eaab80cf6f62f20f4fd1075f972c0
Yin-bao <272214368@qq.com>
2020-07-25 09:07:38 +0800

    Update 反转链表.py

--- 剑指offer/反转链表.py
+++ 剑指offer/反转链表.py
@@ -11,7 +11,7 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         # 首先定义一个反链表头指针
-        rev = None
+        pre = None
         while head:
           temp = head.next
           # 我们首先想到的是将指针指向前一个节点
@@ -20,15 +20,15 @@ class Solution:
           pre = head
           # 然后更新head(head = head.next) 但是head.next已经变了，所以要提前存起来
           head = temp
-        return rev
+        return pre
  
  
  # 当然我们还可以利用python的语言特性，用一行直接就道题前面的四句话
  class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         # 首先定义一个反链表头指针
-        rev = None
+        pre = None
         while head:
             # 使用一行就能将他们完成，很是方便
             head.next, pre, head = pre, head, head.next
-        return rev
+        return pre
